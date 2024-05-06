2f494f9ce3e9d8faf851eed093182e7d6816aa9c
Sena Pekşin <sena@endcozum.com>
2024-05-06 18:14:29 +0300

    Added ExceptionHandler middleware

---
+++ Core.CrossCuttingConcerns/Exceptions/Extensions/ProblemDetailsExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Extensions;
+
+public static class ProblemDetailsExtensions 
+{
+    public static string AsJson<TProblemDetail>(this TProblemDetail details) where  TProblemDetail : ProblemDetails
+    {
+        return JsonSerializer.Serialize(details);
+    }
+ }
---
+++ Core.CrossCuttingConcerns/Exceptions/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Handlers;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Middlewares;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly HttpExceptionHandler _hppExceptionHandler;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+        _hppExceptionHandler = new HttpExceptionHandler();
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsyn(context.Response, ex); 
+        }
+    }
+
+
+    private Task HandleExceptionAsyn(HttpResponse response, Exception exception)
+    {
+        response.ContentType = "application/json";
+        _hppExceptionHandler.Response = response;
+        return _hppExceptionHandler.HandleExceptionAsync(exception);
+    }
+}
