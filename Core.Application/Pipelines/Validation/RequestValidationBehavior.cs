b0cbdfa20305676d40f3ae00b3e591c57c0c7f51
Sena Pekşin <sena@endcozum.com>
2024-05-14 17:50:03 +0300

    commiit pipeline

---
+++ Core.Application/Pipelines/Validation/RequestValidationBehavior.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using MediatR; 
+
+namespace Core.Application.Pipelines.Validation;
+
+public class RequestValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        ValidationContext<object> validationContext = new(request);
+
+        
+    }
+}
