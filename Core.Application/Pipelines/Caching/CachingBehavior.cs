4a5bb7aed78b4869ea19bda420a72243dd9008ac
Sena Pekşin <sena@endcozum.com>
2024-06-10 09:55:20 +0300

    added caching infrastructure

---
+++ Core.Application/Pipelines/Caching/CachingBehavior.cs
@@ -0,0 +1,63 @@
+﻿using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Text;
+using System.Text.Json;
+
+namespace Core.Application.Pipelines.Caching;
+
+public class CachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>, ICachableRequest
+{
+    private readonly CacheSettings _cacheSettings;
+    private readonly IDistributedCache _cache;
+    private readonly ILogger<CachingBehavior<TRequest, TResponse>> _logger;
+
+    public CachingBehavior( IDistributedCache cache, IConfiguration configuration, ILogger<CachingBehavior<TRequest,TResponse>> logger)
+    {
+        _cacheSettings = configuration.GetSection("CacheSettings").Get<CacheSettings>() ?? throw  new InvalidOperationException();
+        _cache = cache;
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (request.ByPassCache)
+        {
+            return await next();
+        }
+
+        TResponse response;
+        byte[]? cachedResponse = await _cache.GetAsync(request.CacheKey, cancellationToken); // cache'den okuyoruz.
+        if (cachedResponse != null)  // eğer cachede data varsa
+        {
+            response = JsonSerializer.Deserialize<TResponse>(Encoding.Default.GetString(cachedResponse)); // cachedaki datayı deserialize yapalım
+            _logger.LogInformation($"Fetched from Cache -> {request.CacheKey}");
+        }
+        else  // cachede data yoksa , hem cache e eklememiz ve döndürmemiz lazım 
+        {
+            response = await getResponseAndAddToCache(request, next, cancellationToken);
+        }
+        return response;
+    }
+
+    private async Task<TResponse?> getResponseAndAddToCache(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        // veritabanından datayı alacağız ve cache ' e ekleyeceğiz.
+
+        TResponse response = await next();
+        TimeSpan slidingExpiration = request.SlidingExpiration ?? TimeSpan.FromDays(_cacheSettings.SlidingExpiration);
+        DistributedCacheEntryOptions cacheOptions = new()
+        {
+            SlidingExpiration = slidingExpiration,
+        };
+
+        byte[] serializeData = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(response));
+
+        await _cache.SetAsync(request.CacheKey, serializeData, cacheOptions,cancellationToken);
+        _logger.LogInformation($"Added to Cache: {request.CacheKey}");
+
+        return response;
+
+    }
+}
---
+++ Core.Application/Pipelines/Caching/ICachableRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Application.Pipelines.Caching;
+
+public interface ICachableRequest
+{
+    string CacheKey { get; }
+    bool ByPassCache { get; }
+    TimeSpan? SlidingExpiration {  get; }
+}
