75eb936c28c57aa57a9cea82f8aed24a70e61911
Sena Pekşin <sena@endcozum.com>
2024-06-12 10:30:52 +0300

    Added FileLogger implementation

---
+++ Core.CrossCuttingConcerns/Logging/LogDetailWithException.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.CrossCuttingConcerns.Logging;
+
+public class LogDetailWithException : LogDetail
+{
+    public string ExceptionMessage { get; set; }
+
+
+    public LogDetailWithException()
+    {
+        ExceptionMessage = String.Empty;
+    }
+
+    public LogDetailWithException(string fullname, string methodName, string user, List<LogParameter> parameters, string exceptionMessage) : base(fullname, methodName, user, parameters)
+    {
+        ExceptionMessage = exceptionMessage; 
+    }
+}
---
+++ Core.CrossCuttingConcerns/Serilog/ConfigurationModels/FileLogConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.CrossCuttingConcerns.Serilog.ConfigurationModels
+{
+    public class FileLogConfiguration
+    {
+        public string FilePath { get; set; }
+
+
+        public FileLogConfiguration()
+        {
+            FilePath = String.Empty;
+        }
+
+        public FileLogConfiguration(string filePath)
+        {
+            FilePath = filePath;
+        }
+    }
+}
