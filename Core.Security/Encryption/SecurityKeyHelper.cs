a139cda99043852c18387b91330e9afe7242416f
Sena Pekşin <sena@endcozum.com>
2024-07-13 23:29:39 +0300

    Created Security Project

---
+++ Core.Security/Encryption/SecurityKeyHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Core.Security.Encryption;
+
+// bize verilen security anahtarını security key e dönüştüren extension olacak.
+public static class SecurityKeyHelper
+{
+    public static SecurityKey CreateSecurityKey(string securityKey) => new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+
+
+}
---
+++ Core.Security/Encryption/SigningCredentialsHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace Core.Security.Encryption;
+
+
+// parametre : security anahtarı ve kullanacağımız algoritma olacak
+// jwt yi imzlayacağımız nesneyi oluşturup döndürüyor olacağız
+public static class SigningCredentialsHelper
+{
+    public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey) => new(securityKey, SecurityAlgorithms.HmacSha512Signature);
+}
